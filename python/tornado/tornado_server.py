768789ba52fad94aa598251750e7fc648131770a
kimroniny <1301862177@qq.com>
2020-05-09 15:52:19 +0800

    update tornado

--- python/tornado/tornado_server.py
+++ python/tornado/tornado_server.py
@@ -5,22 +5,13 @@ import traceback
 import datetime
 import time
 import json, random
-import os, redis, MySQLdb
+import os
 
 import tornado
 import tornado.httpserver, tornado.ioloop, tornado.options
 import tornado.httpclient, tornado.web, tornado.gen
 
-def try_json_loads(body):
-    try:
-        res = json.loads(body)
-    except Exception as e:
-        print(traceback.format_exc())
-        res = {}
-    finally:
-        return res
-
-class audio_class_detect(tornado.web.RequestHandler):
+class Api(tornado.web.RequestHandler):
 
     def initialize(self, args):
         """
@@ -30,33 +21,21 @@ class audio_class_detect(tornado.web.RequestHandler):
 
     def get(self, *args, **kwargs):
         pass
+        
 
     def post(self, *args, **kwargs):
-        try:
-            # self.request.arguments
-            # self.request.body
-            # self.request.body_arguments
-            # self.request.query
-            # self.request.query_arguments
-            request_data = try_json_loads(self.request.body)
-            print("callback receive: {}".format(request_data))
-            self.write("success")
-        except Exception as e:
-            print (traceback.format_exc())
-            self.write("fail")
+        pass
 
 
 def make_app(args):
     return tornado.web.Application([
-        (r"/callback_url", audio_class_detect, dict(args=args)),
+        (r"/api/(.*)", Api, dict(args=args)),
     ])
 
 def getArg():
     parser = argparse.ArgumentParser(description='params for python')
     parser.add_argument('--port', type=int, default=10051)
-    parser.add_argument('--start_num', type=int, default=2)
-    parser.add_argument('--redis_name', type=str, default="")
-    parser.add_argument('--redis_key', type=str, default="callback_data")
+    parser.add_argument('--start_num', type=int, default=1)
     return parser.parse_args()
 
 if __name__ == "__main__":
